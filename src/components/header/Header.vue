93cd6b99f3d94c6446e66c958f8155770f676e32
孙继稳 <2270369261@qq.com>
2023-07-17 15:08:22 +0800

    login

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -194,7 +194,7 @@ export default {
           id: 2,
           num: 12,
           src: require("@/assets/images/Adduser.png"),
-          tittle: "My invoke",
+          tittle: "My download",
         },
         {
           id: 3,
